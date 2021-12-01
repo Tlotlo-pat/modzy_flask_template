5b7b8799e00b87702059941d978781c6fff060a8
Tlotlo-pat <51744364+Tlotlo-pat@users.noreply.github.com>
2021-12-01 20:08:13 +0200

    first commit

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'modzy modelops are incredible'
 
 	API_URL = os.environ.get('API_URL') or "https://app.modzy.com/api"
-	API_KEY = os.environ.get('API_KEY') or "API_KEY_Public.API_KEY_Private"
+	API_KEY = os.environ.get('API_KEY') or "b7rsdiLdtWfxgZ2J8g7f.S9fLI7sBMgzML7rVCDM2"
 	
 	DEBUG =  os.environ.get('FLASK_DEBUG') or 0
